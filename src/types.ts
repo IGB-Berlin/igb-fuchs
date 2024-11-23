13189bb39cd1ef47b30b4251ad67756a153b90a2
Hauke D <haukex@zero-g.net>
2024-11-23 22:55:49 +0100

    Began prototyping data structure

--- src/types.ts
+++ src/types.ts
@@ -30,3 +30,133 @@
  *   - O2mg 0...Sauerstoffkonzentration...14,6 mg L
  *   - Level 0...Pegel...x cm (ohne Komma)
  */
+
+/** Identifier stored as string - TODO Later: enforce format */
+type Identifier = string
+/** Timestamp stored as string - TODO Later: enforce ISO 8601 format */
+type Timestamp = string
+
+/** EPSG:4326 Coordinates ("WGS 84")
+ *
+ * Used by many GPS devices and the API of many online maps, like OSM and Google Maps.
+ * Note KML requires "Lon,Lat" while many others (like Google Maps) require "Lat,Lon".
+ * (Berlin's Lat,Lon is roughly 52.5,13.4)
+ *
+ * Precision: https://gis.stackexchange.com/a/8674 : eight decimal places ~1.1mm, Google Maps now gives six for ~11cm
+ */
+export interface WGS84Coordinates {
+  lat :number
+  lon :number
+}
+
+export type MeasurementTypeId = 'temperature'|'water-level'|'conductivity'|'o2-percent'|'o2-absolute'|'ph'
+export type Units = '°C'|'cm'|'µS/cm'|'%'|'mg/L'|'pH'
+
+interface MeasurementTypeBase<I extends Identifier, U extends string> {
+  custom ?:boolean
+  id :I
+  units :U
+}
+/** Describes a type of measurement (not a specific measurement value). */
+export interface MeasurementType extends MeasurementTypeBase<MeasurementTypeId, Units> {
+  custom :never
+  //TODO Later: Are MeasurementTypeId and Units always linked?
+  id :MeasurementTypeId
+  units :Units
+  /** Optional: Maximum value for input validation. */
+  min ?:number
+  /** Option: Minimum value for input validation. */
+  max ?:number
+  /** Optional: Number of places after the decimal point (both for input validation and for display rounding). */
+  fractionalDigits ?:number
+}
+/** Alternative to `MeasurementType` for use in case a unplanned measurement
+ * needs to be taken, or a measurement type is not yet supported by this app. */
+export interface CustomMeasurement extends MeasurementTypeBase<Identifier, string> {
+  custom :true
+  /** This ID may *not* be a value from `MeasurementTypeId` and *must* be unique. */
+  id :Identifier
+  /** Can be freeform, or existing units from `Units`. */
+  units :string
+  notes ?:string
+}
+
+/** Records an actual recorded measurement. */
+export interface Measurement {
+  /** *Must* reference an identifier from `SamplingTrip.measurementTypes`. */
+  typeId :Identifier
+  time :Timestamp
+  value :number
+}
+
+export type WaterType = 'surface-stream'|'surface-pond'|'ground'|'precipitation'
+
+/** Describes a sample taken as a template type - actual
+ * samples are recorded as `Sample`s. */
+export interface SampleType {
+  waterType :WaterType
+  /** Optional: The typical measurements performed on this sample.
+   * See also `SamplingTripTemplate.typicalMeasurements` if every location is the same. */
+  typicalMeasurements ?:MeasurementType[]
+}
+/** Records an actual sample taken. */
+export interface Sample extends SampleType {
+  measurements :Measurement[]
+  notes ?:string
+  typicalMeasurements :never
+}
+
+/** Describes a sampling location as a template type - actual
+ * measurement sites are recorded as `SamplingPoint`s. */
+export interface SamplingLocation {
+  id :Identifier
+  name :string
+  alternateNames ?:string[]
+  description ?:string
+  normalCoordinates :WGS84Coordinates
+  /** Optional: The typical samples taken at this location,
+   * each of which can have a set of typical measurements. */
+  typicalSamples ?:SampleType[]
+}
+/** Records and actual sampling point. */
+export interface SamplingPoint extends SamplingLocation {
+  startTime :Timestamp
+  endTime :Timestamp
+  actualCoordinates :WGS84Coordinates
+  samples :Sample[]
+  notes ?:string
+  /** Pictures taken at this location - TODO Later: how to represent as JSON? Filenames? */
+  photos ?:string[]
+  typicalSamples :never
+}
+
+/** Describes a sampling trip as a template type - actual
+ * sampling trips are recorded as `SamplingTrip`s. */
+export interface SamplingTripTemplate {
+  /** Name of this sampling trip template; must be unique. */
+  name :string
+  description ?:string
+  /** Optional: The typical points on this sampling trip. */
+  typicalPoints ?:SamplingLocation[]
+  /** Optional: The typical measurements performed at each location.
+   * See also `SamplingLocation.typicalSamples` for per-site specification. */
+  typicalMeasurements ?:MeasurementType[]
+}
+/** Records an entire sampling trip. */
+export interface SamplingTrip extends SamplingTripTemplate {
+  id :Identifier
+  startTime :Timestamp
+  endTime :Timestamp
+  /** Last modification time, in case of any later edits. */
+  lastModified :Timestamp
+  persons :string
+  weather :string
+  notes ?:string
+  points :SamplingPoint[]
+  /** *Must* contain all the measurement types performed on this trip with unique
+   * identifiers; i.e. every `Measurement.typeId` *must* be represented here.
+   * TODO Later: Can also be used at the beginning of the trip to plan which measurements will be offered in UI by default? */
+  measurementTypes :(MeasurementType|CustomMeasurement)[]
+  typicalPoints :never
+  typicalMeasurements :never
+}
