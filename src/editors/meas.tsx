88d5214ddf9002d54eee2d835dd48d8837116e9d
Hauke D <haukex@zero-g.net>
2025-01-19 09:12:47 +0100

    Refactored meas editor for better scrolling

--- src/editors/meas.tsx
+++ src/editors/meas.tsx
@@ -29,38 +29,36 @@ import { Measurement } from '../types/meas'
 import { tr } from '../i18n'
 
 export class MeasurementEditor extends Editor<MeasurementEditor, Measurement> {
-  override readonly currentName
-  protected override readonly form2obj
-  protected override newObj() { return new Measurement(null) }
-
+  private readonly measType :[MeasurementType]
+  private readonly grpType
   private readonly inpValue
+  private readonly inpTime
   constructor(parent :EditorParent, targetStore :AbstractStore<Measurement>, targetObj :Measurement|null) {
     super(parent, targetStore, targetObj)
-    const obj = this.initObj
 
-    const measType :[MeasurementType] = [obj.type]
-    const mtStore = new ArrayStore(measType)
+    this.measType = [this.initObj.type]
+    const mtStore = new ArrayStore(this.measType)
     const inpType = safeCastElement(HTMLInputElement, <input type="text" class="form-control fw-semibold" value="" readonly required /> )
     const btnTypeEdit = <button type="button" class="btn btn-outline-primary"><i class="bi-pencil"/> {tr('Edit')}</button>
     btnTypeEdit.addEventListener('click', () => {
-      const ed = new MeasTypeEditor(this, mtStore, measType[0])
+      const ed = new MeasTypeEditor(this, mtStore, this.measType[0])
       ed.el.addEventListener(CustomStoreEvent.NAME, typeChange)  // typeChange bubbles the event
     })
     const btnTypeSel = <button type="button" class="btn btn-outline-primary"><i class="bi-card-list"/> {tr('Select')}</button>
     btnTypeSel.addEventListener('click', async () => {
       const type = await listSelectDialog(tr('sel-meas-type'), this.ctx.storage.allMeasurementTemplates)
       if (type) {
-        await mtStore.upd(measType[0], type)
+        await mtStore.upd(this.measType[0], type)
         typeChange()
       }
     })
-    const grpType = safeCastElement(HTMLDivElement, <div class="input-group"> {inpType} {btnTypeEdit} {btnTypeSel} </div>)
+    this.grpType = safeCastElement(HTMLDivElement, <div class="input-group"> {inpType} {btnTypeEdit} {btnTypeSel} </div>)
 
     const typeInst = safeCastElement(HTMLTextAreaElement, <textarea rows="2" readonly></textarea>)
     const rowInst = this.makeRow(typeInst, tr('Instructions'), <>{tr('meas-inst-help')}</>, null)
 
     this.inpValue = safeCastElement(HTMLInputElement, <input type="text" inputmode="decimal"
-      class="form-control fw-semibold font-monospace" value={obj.value} required />)
+      class="form-control fw-semibold font-monospace" value={this.initObj.value} required />)
     numericTextInputStuff(this.inpValue)
     this.inpValue.addEventListener('change', () => grpValue.dispatchEvent(new CustomChangeEvent()))
     const lblUnit = <span class="input-group-text"></span>
@@ -68,39 +66,52 @@ export class MeasurementEditor extends Editor<MeasurementEditor, Measurement> {
     const lblRange = <span></span>
     const lblPrc = <span></span>
 
-    const inpTime = new DateTimeInput(obj.time, true)
+    this.inpTime = new DateTimeInput(this.initObj.time, true)
 
     const typeChange = () => {
-      inpType.value = measType[0].name
-      if (measType[0].unit.trim().length) lblUnit.innerText = measType[0].unit
+      const mt = this.measType[0]
+      inpType.value = mt.name
+      if (mt.unit.trim().length) lblUnit.innerText = mt.unit
       else lblUnit.replaceChildren(<em>({tr('no units')})</em>)
-      const r = measType[0].rangeAsText
+      const r = mt.rangeAsText
       if (r.length) lblRange.innerText = r
       else lblRange.replaceChildren(<em>({tr('not specified')})</em>)
-      const p = measType[0].precision
+      const p = mt.precision
       lblPrc.innerText = Number.isFinite(p) && p>=0 ? `; ${tr('precision')} ${p}` : ''
-      typeInst.value = measType[0].instructions
-      rowInst.classList.toggle('d-none', !measType[0].instructions.trim().length)
-      this.inpValue.pattern = makeValidNumberPat(measType[0].precision)
-      grpType.dispatchEvent(new CustomChangeEvent())
+      typeInst.value = mt.instructions
+      rowInst.classList.toggle('d-none', !mt.instructions.trim().length)
+      this.inpValue.pattern = makeValidNumberPat(mt.precision)
+      this.grpType.dispatchEvent(new CustomChangeEvent())
       this.el.dispatchEvent(new CustomStoreEvent({ action: 'upd', id: '0' }))  // essentially a bubbling of the event (see above)
     }
     typeChange()
 
-    this.form2obj = () => new Measurement({ type: measType[0],
-      value: this.inpValue.value, time: inpTime.timestamp })
-    this.currentName = () => measType[0].name
-
     this.initialize([
-      this.makeRow(grpType, tr('Measurement Type'), <><strong>{tr('Required')}.</strong> {tr('meas-type-help')}</>, tr('Invalid measurement type')),
+      this.makeRow(this.grpType, tr('Measurement Type'), <><strong>{tr('Required')}.</strong> {tr('meas-type-help')}</>, tr('Invalid measurement type')),
       rowInst,
       this.makeRow(grpValue, tr('Value'),
         <><strong>{tr('Required')}.</strong> {tr('meas-value-help')} {lblRange}{lblPrc}. {tr('dot-minus-hack')}</>, tr('Invalid value')),
-      this.makeRow(inpTime.el, tr('Timestamp'), <><strong>{tr('Required')}.</strong> {tr('meas-time-help')}</>, tr('Invalid timestamp')),
+      this.makeRow(this.inpTime.el, tr('Timestamp'), <><strong>{tr('Required')}.</strong> {tr('meas-time-help')}</>, tr('Invalid timestamp')),
     ])
   }
+
+  protected override newObj() { return new Measurement(null) }
+
+  protected override form2obj() {
+    return new Measurement({ type: this.measType[0],
+      value: this.inpValue.value, time: this.inpTime.timestamp })
+  }
+
+  override currentName() { return this.measType[0].name }
+
+  protected override doScroll() {
+    this.ctx.scrollTo( !this.measType[0].name ? this.grpType : this.inpValue )
+  }
+
   override shown(pushNotPop :boolean) {
     super.shown(pushNotPop)
-    this.inpValue.focus()
+    if ( this.measType[0].name && !this.inpValue.value )
+      this.inpValue.focus()
   }
+
 }
