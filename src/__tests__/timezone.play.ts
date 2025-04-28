bb6f61ffe6d730f0e5eba0341e17b942f8d5e212
Hauke D <haukex@zero-g.net>
2025-04-28 11:44:50 +0200

    [WIP playwright] Hopefully fix TZ test issue

--- src/__tests__/timezone.play.ts
+++ src/__tests__/timezone.play.ts
@@ -21,8 +21,8 @@ import { test, expect } from '@playwright/test'
 test.use({ timezoneId: 'UTC' })
 test('different timezone', async ({ page }) => {
   await initPageTest(page)
-  //await page.emulateMedia({ reducedMotion: 'reduce' })  // this seems to help in WebKit
-  await page.clock.setFixedTime('2024-01-02T02:01')
+  await page.emulateMedia({ reducedMotion: 'reduce' })  // this seems to help in WebKit
+  await page.clock.setFixedTime('2024-01-02T01:01Z')
 
   // Create a new Sampling Log
   await expect(page.getByTestId('accSampLog')).toBeVisible()
@@ -34,20 +34,24 @@ test('different timezone', async ({ page }) => {
   await expect(page.getByTestId('logEndTime').getByRole('textbox')).toHaveValue('')
 
   // Set Log times
-  await page.clock.setFixedTime('2024-01-02T03:02')  // log start time
+  await page.clock.setFixedTime('2024-01-02T02:02Z')  // log start time
   await page.getByTestId('logStartTime').getByRole('button').click()
   await expect(page.getByRole('button', { name: 'Use current time' })).toBeVisible()
   await page.getByRole('button', { name: 'Use current time' }).click()
-  //TODO NEXT: In WebKit, the following and even typing the DT string into the text box doesn't work, but the "Use current time" button does??
-  //await page.getByTestId('logEndTime').getByRole('textbox').fill('2024-01-02T06:05')
-  //await page.getByTestId('logEndTime').getByRole('textbox')
-  //  .evaluate((input :HTMLInputElement, value :string) => input.value = value, '2024-01-02T06:05')
-  await page.clock.setFixedTime('2024-01-02T09:08')  // dummy for debugging
-  return  //TODO: remove me
+  await expect(page.getByTestId('logStartTime').getByRole('textbox')).toHaveValue('2024-01-02T02:02')
+
+  const isWebkit = page.context().browser()?.browserType().name() === 'webkit'
+  await page.clock.setFixedTime('2024-01-02T05:05Z')
+  if (isWebkit) { // WebKit apparently can't parse manually entered date/times...?
+    await page.getByTestId('logEndTime').getByRole('button').click()
+    await expect(page.getByRole('button', { name: 'Use current time' })).toBeVisible()
+    await page.getByRole('button', { name: 'Use current time' }).click()
+  }
+  else await page.getByTestId('logEndTime').getByRole('textbox').fill('2024-01-02T05:05')
+  await expect(page.getByTestId('logEndTime').getByRole('textbox')).toHaveValue('2024-01-02T05:05')
   await page.getByRole('button', { name: /Save\s*$/ }).click()
 
   // Sampling Log needs at least one Location for export
-  await page.clock.setFixedTime('2024-01-02T05:04')  // location end time
   await page.getByRole('button', { name: 'New' }).click()
   await expect(page.getByRole('heading', { name: 'Sampling Location' })).toBeVisible()
   await page.getByRole('textbox', { name: 'Name' }).fill('S1')
@@ -55,12 +59,19 @@ test('different timezone', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Longitude' }).fill('13.1')
 
   // Set Location times
-  await page.getByTestId('locStartTime').getByRole('textbox').fill('2024-01-02T04:03')
+  await page.clock.setFixedTime('2024-01-02T03:03Z')
+  if (isWebkit) {  // same as above
+    await page.getByTestId('locStartTime').getByRole('button').click()
+    await expect(page.getByRole('button', { name: 'Use current time' })).toBeVisible()
+    await page.getByRole('button', { name: 'Use current time' }).click()
+  }
+  else await page.getByTestId('locStartTime').getByRole('textbox').fill('2024-01-02T03:03')
+  await expect(page.getByTestId('locStartTime').getByRole('textbox')).toHaveValue('2024-01-02T03:03')
+  await page.clock.setFixedTime('2024-01-02T04:04Z')  // location end time
   await expect(page.getByRole('checkbox', { name: 'Automatically set end time' })).not.toBeChecked()
   await page.getByRole('checkbox', { name: 'Automatically set end time' }).check()
 
   // Finish sampling log
-  return
   await page.getByRole('button', { name: 'Save & Close' }).click()
   await expect(page.getByRole('heading', { name: 'Sampling Log' })).toBeVisible()
   await page.getByRole('button', { name: 'Back' }).focus()  // Workaround for stack bug
@@ -68,13 +79,13 @@ test('different timezone', async ({ page }) => {
   await expect(page.getByTestId('accSampLog')).toBeVisible()
 
   // Export
-  await page.clock.setFixedTime('2024-01-02T07:06')  // export time
+  await page.clock.setFixedTime('2024-01-02T06:06Z')  // export time
   await page.getByTestId('accSampLog').getByRole('button', { name: 'Export' }).click()
   const downloadPromise = page.waitForEvent('download')
   await page.getByRole('button', { name: 'As CSV' }).click()
   const expFile = await dl2file(await downloadPromise)
   console.log(expFile.name)
   const csv = await expFile.text()
-  console.log(csv)
+  console.log(csv)  //TODO: Inspect CSV File!
 
 })
