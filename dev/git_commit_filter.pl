368fa9541b17e545d8bf24c350c3e8be79a7dc4f
Hauke D <haukex@zero-g.net>
2025-10-23 12:36:13 +0200

    Fix "use open" usage

--- dev/git_commit_filter.pl
+++ dev/git_commit_filter.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use open qw/ :std :raw :encoding(UTF-8) /;
+use open ':std', ':raw:encoding(UTF-8)';
 
 my $CMD = shift // '';
 my $repl = '$Commit$';  # default for clean
