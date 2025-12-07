9678e46369af0d42b1aba871a3a293664900d825
Hauke D <haukex@zero-g.net>
2025-12-07 22:43:01 +0100

    Ensure setup_git_filter.sh works on older Git

--- setup_git_filter.sh
+++ setup_git_filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
-git config set --local filter.git_commit.clean "\$PWD/dev/git_commit_filter.pl clean"
-git config set --local filter.git_commit.smudge "\$PWD/dev/git_commit_filter.pl smudge"
+git config --local filter.git_commit.clean "\$PWD/dev/git_commit_filter.pl clean"
+git config --local filter.git_commit.smudge "\$PWD/dev/git_commit_filter.pl smudge"
 perl -i dev/git_commit_filter.pl smudge worker/service-worker.ts src/main.ts
