ee260814e932fcd2359c0f800423a0857a4efc87
Hauke D <haukex@zero-g.net>
2026-01-10 18:16:45 +0100

    Limit git filters to this worktree
    [skip actions]

--- dev/setup_git_filter.sh
+++ dev/setup_git_filter.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/..
 
-# Sets up git_commit_filter.pl (handles $Commit$)
+# Sets up git_commit_filter.pl (which handles $Commit$)
 
-git config --local filter.git_commit.clean "\$PWD/dev/git_commit_filter.pl clean"
-git config --local filter.git_commit.smudge "\$PWD/dev/git_commit_filter.pl smudge"
+git config --worktree filter.git_commit.clean "\$PWD/dev/git_commit_filter.pl clean"
+git config --worktree filter.git_commit.smudge "\$PWD/dev/git_commit_filter.pl smudge"
 perl -i dev/git_commit_filter.pl smudge worker/service-worker.ts src/main.ts
